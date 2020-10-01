f572f8be92ee8b7d33e5b92263a960c7fd86754b
Al <alex2015mig@gmail.com>
2020-10-02 01:18:04 +0300

    'news_render'

--- src/index.js
+++ src/index.js
@@ -1,9 +1,43 @@
 import './index.scss';
 
-async function log() {
-  const resp = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-  console.log(resp);
-  return resp;
+const API_KEY = 'e8b3c5ed-fcae-444c-b93e-e45436c12c38';
+const url = `https://content.guardianapis.com/search?api-key=${API_KEY}`;
+
+const list = document.getElementById('list');
+const loader = document.getElementById('loader');
+const refreshBtn = document.getElementById('refresh');
+
+async function getNews() {
+    const result = await fetch(url)
+        .then((res) => res.json())
+        .then(({ response }) => response.results);
+    return result;
 }
 
-log();
+function insertNewsElement(text) {
+    const element = document.createElement('li');
+    element.insertAdjacentText('beforeend', text);
+    list.insertAdjacentElement('beforeend', element);
+}
+
+async function renderNews() {
+    list.innerHTML = '';
+    loader.classList.toggle('hidden-block');
+    try {
+        await getNews(url)
+            .then((news) => {
+                news.forEach(({ webTitle }) => {
+                    insertNewsElement(webTitle);
+                });
+            });
+    } catch (e) {
+        const warningElement = document.createElement('li');
+        warningElement.insertAdjacentText('beforeend', e.message);
+        warningElement.classList.add('text-danger', 'error');
+        list.insertAdjacentElement('beforeend', warningElement);
+    }
+    loader.classList.toggle('hidden-block');
+}
+
+refreshBtn.addEventListener('click', renderNews);
+renderNews();
